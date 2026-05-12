bd286d535f55412a4324f833f28be869e01a09d2
onlyifido <onlyifido@gmail.com>
2026-05-13 00:44:54 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,3 @@
-// 기본적인 PWA 설치를 위한 빈 서비스 워커
 self.addEventListener('install', (e) => {
   self.skipWaiting();
 });
@@ -8,5 +7,4 @@ self.addEventListener('activate', (e) => {
 });
 
 self.addEventListener('fetch', (e) => {
-  // 네트워크 요청을 가로채지 않고 그대로 통과시킴
 });
